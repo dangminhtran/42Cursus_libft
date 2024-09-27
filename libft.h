ac1c8180c2dbbb6cbf77ae73832fc419eb677600
Dang-Minh TRAN <dang-minh.tran@hotmail.com>
2024-09-27 21:58:58 +0200

    first functions

---
+++ libft.h
@@ -0,0 +1,13 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+
+
+#endif
